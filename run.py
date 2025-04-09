d45c3f9fcbcb98fe2267c5d85ce1466ca2754905
Bitsave <megasley@freerouting.africa>
2025-04-09 03:24:12 +0000

    first commit

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from app import app
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+from app import app  # or however you're importing your Flask app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 if PORT isn't set
+    app.run(host="0.0.0.0", port=port, debug=True)
